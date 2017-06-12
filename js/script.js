9e2e35918c18227f6f83c784764fc19ee534f540
Artem Makarov <artemailcz@gmail.com>
2017-06-12 19:07:27 +0200

    2
    little scripts + arrow

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+   
+   $("#nadpis").fadeIn(1500);
+   $("#motto").fadeIn(2500);
+   $("#arrow").fadeIn(3500);
+});
+
+
+
+
+$("#para").click(function() {
+$("#para").hide();
+
+});
+
+$("#motto").click(function() {
+    alert("lol poser si boty ty sracko vole");
+});
+
